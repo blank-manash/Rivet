783012c0cb32c87860fca12a19aad1193e1ee122
manash <manash@iitk.ac.in>
2022-03-17 01:50:10 +0530

    Added Help HTML Page

--- src/main/java/com/demo/application/controllers/ApplicationController.java
+++ src/main/java/com/demo/application/controllers/ApplicationController.java
@@ -3,6 +3,7 @@ package com.demo.application.controllers;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,11 @@ import lombok.extern.slf4j.Slf4j;
 public class ApplicationController {
 
 	private UserService userService;
+
+	@GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+	public String welcome() {
+		return "<html><head><title>Page Title</title></head><body><h1>Rivet Backend</h1><ul><li><code>/find-users<code> : Find with filters <li><code>/find-friends<code> : Request Param is currentUser <li><code>/search-by-tags<code> : Request Param is tags=1,2,3 <li><code>/register-user<code> : Request Body is User JSON Body <li><code>/update-user<code> : Request Body is User JSON Body <li><code>/add-tags<code> : Request Param is id_a=1 & id_b=2 <li><code>/block-user<code> Request Param id_a, id_b <li><code>/unblock-user<code> : Same as block user <li><code>/add-friend<code> : Same as block user <li><code>/remove-friend<code> : You got it, same- id_a, id_b <li><code>/add-tags<code> : Request Param is id=1 & tag=\"Music, Sports, Academics\"<ul></body></html>";
+	}
 
 	@GetMapping("/find-user")
 	public List<User> getUsersWith(@RequestParam Map<String, String> params) {
@@ -54,7 +60,7 @@ public class ApplicationController {
 	}
 
 	@PostMapping("/add-tags")
-	public boolean addTags(@RequestParam("id_a") Long idA, @RequestParam("tag") String tag) {
+	public boolean addTags(@RequestParam("id") Long idA, @RequestParam("tag") String tag) {
 		return userService.addTags(idA, tag);
 	}
 
@@ -79,7 +85,7 @@ public class ApplicationController {
 	}
 
 	@DeleteMapping("/remove-tags")
-	public boolean removeTags(@RequestParam("id_a") Long idA, @RequestParam("tag") String tag) {
+	public boolean removeTags(@RequestParam("id") Long idA, @RequestParam("tag") String tag) {
 		return userService.removeTags(idA, tag);
 	}
 
