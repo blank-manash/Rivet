a606ed873384338c6499c1e48f723151c0302372
manash <manash@iitk.ac.in>
2022-04-01 16:30:59 +0530

    Import Fixes

--- src/main/java/com/demo/application/controllers/ApplicationController.java
+++ src/main/java/com/demo/application/controllers/ApplicationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
